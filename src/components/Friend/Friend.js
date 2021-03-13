e9033a8982c39a6f4eace0d34dfab0676cf886d0
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-03-14 00:08:48 +0600

    react-router practice day 1

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+const Friend = (props) => {
+    const {name, email,id} = props.friend;
+    const history = useHistory();
+    const handleClick = (friendId) => {
+        history.push(`/friend/${friendId}`)
+    }
+    const friendStyle= {
+        border: '1px solid purple',
+        margin: '20px',
+        padding: '20px',
+        borderRadius:'20px'
+    }
+    return (
+        <div style={friendStyle}>
+            <h2>Name:{name}</h2>
+            <p>Email: {email}</p>
+            <Link to={`/friend/${id}`}>
+                <button>Show Details {id} </button>
+            </Link>
+            <button onClick={()=>handleClick(id)}>Click Me</button>
+        </div>
+    );
+};
+
+export default Friend;
